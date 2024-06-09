77701ab244a7af186499549c31b9d8367ee31afb
Samuel Le <robbo_rules1@live.com.au>
2024-06-09 13:41:23 +1000

    Setup basic page with html and css

--- reddit/src/App.js
+++ reddit/src/App.js
@@ -2,10 +2,23 @@
 function App() {
   return (
     <div className="App">
-      <div className="border-solid border-2 border-sky-500">
-        <h1 className="text-9xl">Reddit App</h1>
+      <div className="text-center h-20 bg-slate-900" >
+        <h1 className="text-white text-5xl pt-2.5">Reddit App</h1>
       </div>
-      <button className="w-10 h-10">kdklsfahjlk;sd</button>
+
+      <div id="content" className="flex flex-row justify-evenly bg-slate-800">
+        <div id="postDisplay" className=" text-center w-4/5 m-5 border">
+          <h2 className="text-white">Display</h2>
+        </div>
+
+        <div id="navBar" className="text-center w-1/2 h-screen m-5 shadow-2xl rounded-3xl bg-slate-950">
+          <h2 className="text-slate-600 pt-2.5">SubReddit</h2>
+        </div>
+      </div>
+      
+      
+
+      
     </div>
   );
 }
