60ec395240a40490b955cd484ee6509a5626245e
Samuel Le <robbo_rules1@live.com.au>
2024-06-11 11:43:42 +1000

    Created postSlice

---
+++ reddit/src/Slices/postSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+// get data
+
+const initialState = {
+    Posts: [
+        {
+            id: 1,
+            author: "Jack",
+            title: "TITLE",
+            content: 'Insert post here',
+            likes: 20000,
+            timePosted: 2,
+            isCommentToggled: false,
+            comments: [
+                {
+                    content: "hello",
+                    author: 'David',
+                    likes: 10,
+                    timePosted: 1,
+                }
+            ]
+        },
+    ]
+}
+
+const postSlice = createSlice({
+    name: 'post',
+    initialState: initialState,
+    reducers: {
+        toggleComment: (state, action) => {
+            const postToBeToggled = state.find(post => post.id === action.payload.id);
+            if (postToBeToggled) {
+                postToBeToggled.isCommentToggled = !postToBeToggled.isCommentToggled;
+            }
+        }
+    }
+})
+
+export const {toggleComment} = postSlice.actions;
+export default postSlice.reducer;
