9ff47d902e314892cd040f65d070f99aa9a23813
Samuel Le <robbo_rules1@live.com.au>
2024-06-11 11:43:57 +1000

    Created store

--- reddit/src/index.js
+++ reddit/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
+import { configureStore } from "@reduxjs/toolkit";
+import postReducer from './Slices/postSlice.js';
+
 import './index.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store=
+  {
+    configureStore({
+      reducer: {
+          post: postReducer,
+      },
+    })
+  }>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 
